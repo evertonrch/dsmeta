74b34c0b5b2a72d5c26d619358a11c56fb2036ae
Everton <irocha10@hotmail.com>
2022-11-18 10:11:39 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -3,13 +3,14 @@ package com.devsuperior.dsmeta.controller;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
 @Controller
@@ -20,7 +21,10 @@ public class SaleController {
 
     @GetMapping("all")
     @ResponseBody
-    public List<Sale> sales() {
-        return saleService.sales();
+    public Page<Sale> sales(
+            @RequestParam(value = "from", defaultValue = "") String minDate,
+            @RequestParam(value = "to", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return saleService.sales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
@@ -1,13 +1,19 @@
 package com.devsuperior.dsmeta.repository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT s FROM Sale s WHERE s.date BETWEEN :min AND :max ORDER BY s.amount DESC")
+    Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
