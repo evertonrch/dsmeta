e35c8f010014393704602ee2000a1383da562dcc
Everton <irocha10@hotmail.com>
2022-11-19 02:11:36 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,20 +1,20 @@
-import axios from 'axios';
-import icon from '../../assets/img/notification-icon.svg';
-import { BASE_URL } from '../../utils/request';
-import './styles.css';
+import axios from "axios";
+import { toast } from "react-toastify";
+import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
 
 type Props = {
   saleId: number;
+};
+
+function handleClick(id: number) {
+  axios.get(`${BASE_URL}/sale/${id}/notification`).then((response) => {
+    toast.info("SMS enviado com sucesso!");
+  });
 }
 
-function handleClick(id : number) {
-  axios.get(`${BASE_URL}/sale/${id}/notification`)
-        .then(response => {
-          console.log(response.data.content);
-        })
-}
-
-function NotificationButton({saleId} : Props) {
+function NotificationButton({ saleId }: Props) {
   return (
     <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
